a8559547ec1f792568b956026c00e0ff181efeae
lisa jones <lisadjones75@gmail.com>
2024-05-08 16:05:27 +0000

    Set up heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-we: gunicorn django_todo.wsgi:application
+web: gunicorn django_todo.wsgi:application
--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+if os.path.isfile('env.py'):
+    import env
+os.environ.get("DATABASE_URL")
+
+DATABASES = {'default':dj_database_url.parse('postgres://DATABASE_URL')}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +81,12 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
